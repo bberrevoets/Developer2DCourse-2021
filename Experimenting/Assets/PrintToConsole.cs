d5175b2119f6e4787ab79b696a9815490c968c1b
Bert Berrevoets <bert@berrevoets.net>
2022-03-27 10:01:40 +0200

    Setup Visual Studio: Done!

---
+++ Experimenting/Assets/PrintToConsole.cs
@@ -0,0 +1,26 @@
+// =====================================================================
+// Author: Bert Berrevoets (bberr)
+// 
+// Created  : 27-03-2022    09:50
+// Updated  : 27-03-2022    09:53
+// 
+// Solution: Experimenting
+// Project:  Assembly-CSharp
+// Filename: PrintToConsole.cs
+// =====================================================================
+
+using UnityEngine;
+
+public class PrintToConsole : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private void Start()
+    {
+        Debug.Log("Ate.. too... many... donuts...");
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 Developer2DCourse-2021
+
+[Next Video](https://www.gamedev.tv/courses/1394720/lectures/32170343 "Next Video")
